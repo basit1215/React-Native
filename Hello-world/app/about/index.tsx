3e646e5b2dfd953c3ad3c905ed9cd87f5ec32fb4
malik1243 <154369335+basit1215@users.noreply.github.com>
2024-10-17 13:39:38 +0500

    updated

--- Hello-world/app/about/index.tsx
+++ Hello-world/app/about/index.tsx
@@ -1,10 +1,46 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 
 const About = () => {
   return (
-    <View>
-      <Text>About</Text>
+    <View style={{
+      paddingVertical: 30,
+      gap: 50,
+      backgroundColor: 'lightblue',
+      flex: 1
+
+    }}>
+      <View style={{
+        backgroundColor: 'brown',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 80,
+        borderRadius: 25,
+        width: 300,
+        marginHorizontal: 30
+      }}>
+        <Text style={{
+          color: 'white',
+          fontSize: 30
+        }}>About</Text>
+      </View>
+
+      <View  style={{
+        backgroundColor: 'orange',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 80,
+        borderRadius: 25,
+        width: 300,
+        marginHorizontal: 30
+      }}>
+        <Link style={{
+          color: 'white',
+          fontSize: 30
+        }} href={'/'}>Back to Home</Link>
+      </View>
+
     </View>
   )
 }
--- Hello-world/app/index.tsx
+++ Hello-world/app/index.tsx
@@ -3,19 +3,54 @@ import React from 'react'
 import { Link } from 'expo-router'
 
 const Home = () => {
-  return (
-    <>
-     <View>
-      <Text>Home</Text>
-      <Link href={"about"}>About</Link>
-      <Link href={"contact"}>Contact</Link>
-      <Link href={"service"}>Service</Link>
-    </View>
+    return (
+        <View style={{
+            backgroundColor: 'lightblue',
+            flex: 1
+        }}>
+            <View style={{
+                flexDirection: 'row',
+                backgroundColor: 'pink',
+                justifyContent: 'space-around',
+                alignItems: "center",
+                gap: 30,
+                width: 360,
+                height: 60,
+                marginBottom: 180,
+            }}>
+                <Link style={{
+                    fontSize: 20,
+                    fontWeight: 'bold',
+                    color: 'purple'
+                }} href={'/about'}>About</Link>
+                <Link style={{
+                    fontSize: 20,
+                    fontWeight: 'bold',
+                    color: 'purple'
+                }} href={'/contact'}>Contact</Link>
+                <Link style={{
+                    fontSize: 20,
+                    fontWeight: 'bold',
+                    color: 'purple'
+                }} href={'/service'}>Service</Link>
+            </View>
 
-
-
-    </>
- )
+            <View style={{
+                backgroundColor: 'brown',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: 80,
+                borderRadius: 25,
+                width: 300,
+                marginHorizontal: 30
+            }}>
+                <Text style={{
+                    color: 'white',
+                    fontSize: 30
+                }}>Hello World</Text>
+            </View>
+        </View>
+    )
 }
 
 export default Home
